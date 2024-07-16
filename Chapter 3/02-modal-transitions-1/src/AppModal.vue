06695d07cbcea4f9efd8d4033c7d254b442cec45
Carles Torrubiano <carlestorru@gmail.com>
2024-07-16 22:09:15 +0200

    chapter 3

---
+++ Chapter 3/02-modal-transitions-1/src/AppModal.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { XMarkIcon } from '@heroicons/vue/20/solid';
+defineProps({
+	title: { type: String, default: null },
+	show: { type: Boolean, default: false },
+});
+
+defineEmits(['close']);
+</script>
+
+<template>
+	<Transition>
+		<div class="modal-wrapper" v-if="show">
+			<div class="modal-wrapper-inner">
+				<button class="modal-close-button" @click="$emit('close')">
+					<XMarkIcon class="w-8 h-8" />
+				</button>
+				<h3 class="modal-title" v-if="title">{{ title }}</h3>
+				<div class="modal-inner">
+					<slot />
+				</div>
+			</div>
+		</div>
+	</Transition>
+</template>
+
+<style scoped>
+.v-enter-from,
+.v-leave-to {
+	opacity: 0;
+}
+
+.v-enter-active,
+.v-leave-active {
+	transition: all 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+	transform: translateY(-50px);
+}
+</style>
